107172030b83eebcd44c4d6ccf817269c1aa7a32
Chris Halogen <christackoms@gmail.com>
2022-07-02 10:06:16 +0000

    Mandatory tasks push

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ * Return: Always 0 (sucess)
+ * Description: This function is just a regular c main function
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
