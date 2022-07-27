aa9aef7ac697a1fa3d942a636d17299e19c4ede9
Chris Halogen <christackoms@gmail.com>
2022-07-27 23:29:57 +0000

    Second Problem Unused Variable Fixed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,9 +5,10 @@
  * @argv: arguments value
  * Return: 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	if (argc > 0)
 		printf("%d\n", argc - 1);
+
 	return (0);
 }
