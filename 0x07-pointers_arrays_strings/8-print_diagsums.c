6e696bb67c94830bf49d70428f0f65f1a8f3fe75
Chris Halogen <christackoms@gmail.com>
2022-07-27 09:38:43 +0000

    07 Last number Compulsory

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,21 +2,19 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - print the sums of the 2 diagonals
- * of a matrix
- * @a: a pointer to the matrix
- * @size: the size of the matrix
+ * print_diagsums - print the diagonals of 2 matrices
+ * @a: the pointer to the matrix
+ * @size: the size of the string
  */
+
 void print_diagsums(int *a, int size)
 {
-	int count;
-	int sum1 = 0;
-	int sum2 = 0;
+	int i, sum1 = 0, sum2 = 0;
 
-	for (count = 0; count < size; count++)
+	for (i = 0; i < size; i++)
 	{
-		sum1 += *(a + (size * count + count));
-		sum2 += *(a + (size * count + size - 1 - i));
+		sum1 += *(a + (size * i + i));
+		sum2 += *(a + (size * i + size - 1 - i));
 	}
 	printf("%d, ", sum1);
 	printf("%d\n", sum2);
