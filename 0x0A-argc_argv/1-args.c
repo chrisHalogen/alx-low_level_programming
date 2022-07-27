1c25a6ea8aff47f3787f14f95084cec61e417df5
Chris Halogen <christackoms@gmail.com>
2022-07-27 23:24:17 +0000

    Commit Second Problem

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function to print the number of args
+ * @argc: arguments count
+ * @argv: arguments value
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
