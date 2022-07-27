89ae4e658eeb1f26de4b6ee35633ac3d5310db1d
Chris Halogen <christackoms@gmail.com>
2022-07-27 23:48:10 +0000

    3-mul soln

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - function to multiply 2 numbers
+ * @argc: arguments count
+ * @argv: arguments value
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int count, result = 0;
+
+	for (count = 0; count < argc; count++)
+		count *= atoi(argv[count]);
+
+	printf("%d\n", result);
+
+	return (0);
+}
