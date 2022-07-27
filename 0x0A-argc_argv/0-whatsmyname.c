5dfad32045d50dbb00554871a538b2a2dfcc0ec2
Chris Halogen <christackoms@gmail.com>
2022-07-27 23:14:02 +0000

    Argc Argv First Push

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function to print it's name
+ * @argc: arguments count
+ * @argv: arguments value
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
+
