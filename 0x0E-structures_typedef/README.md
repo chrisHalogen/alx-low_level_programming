3a3b28c1fdfd64fe6f2f842d580326283bce2f06
Chris Halogen <christackoms@gmail.com>
2022-08-02 21:32:28 +0000

    Type Def first commit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+# Structures and Typedef
+
+This directory has all the solution to typefef and Structures problems
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+
+#define DOG_H
+
+/**
+ * struct dog - the dog structure
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+
+
+#endif
