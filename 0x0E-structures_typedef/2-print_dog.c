0e418066eb9351f532f9087dff7dfd597bb72753
Chris Halogen <christackoms@gmail.com>
2022-08-02 22:29:01 +0000

    Trial Push

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,21 +3,17 @@
 #include "dog.h"
 
 /**
- * print_dog - Prints a dog structure entered
- * @d: pointer to the dog structure
+ * print_dog - Prints a struct dog
+ * @d: pointer to struct dog
  */
-
 void print_dog(struct dog *d)
 {
-	if (d != null)
+	if (d != NULL)
 	{
 		printf("Name: ");
 		d->name == NULL ? printf("(nil)\n") : printf("%s\n", d->name);
-
-		printf("Age: %d\n", d->age);
-
+		printf("Age: %f\n", d->age);
 		printf("Owner: ");
 		d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
-
 	}
 }
