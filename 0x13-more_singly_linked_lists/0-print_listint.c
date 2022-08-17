bef0b352607f93b92d730ffcf57f609c548ba2fe
Chris Halogen <christackoms@gmail.com>
2022-08-17 20:24:28 +0000

    Advanced SLL Again

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * print_listint - prints the elements in a linked list
@@ -13,7 +12,8 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		_putchar(h->n);
+		_putchar('\n');
 		h = h->next;
 		count++;
 	}
