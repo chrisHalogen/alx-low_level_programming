8c84116b92bcbfce929c95737c143334283b7f7b
Chris Halogen <christackoms@gmail.com>
2022-10-04 06:43:54 +0000

    Dynamic Libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,50 @@
+/**
+ * add - addition operation
+ * @a: first operand
+ * @b: second operand
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtraction operation
+ * @a: first operand
+ * @b: second operand
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplication
+ * @a: number
+ * @b: multiplier
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - division operation
+ * @a: dividend
+ * @b: divisor
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - modular dividion operation
+ * @a: dividend
+ * @b: divisor
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,2 @@
+# Dynamic Libraries
+This directory has all dynamic libraries answers
